d52d070eb75862c4b30dbe9e66cf11b448050ec5
igoro00 <igor.ordecha@gmail.com>
2025-06-01 04:58:58 +0200

    ok przyzekam to juz ostatni raz jak kombinuje z headereami cppchecka

--- .github/scripts/checkmisra.sh
+++ .github/scripts/checkmisra.sh
@@ -9,21 +9,35 @@ mkdir -p build/cppcheck
 
 cwd=$(pwd)
 jq --arg cwd "$cwd" \
-    -c '[.[] | select((.file | startswith($cwd + "/src")) or (.file | startswith($cwd + "/lib")))]' \
+    -c '[.[] | select((.file | startswith($cwd + "/src")) or (.file | startswith($cwd + "/lib/Wbudy")))]' \
     build/compile_commands.json > build/cppcheck/compile_commands.json
+
+# Add absolute -Imisra/include to each compile command
+jq --arg cwd "$cwd" 'map(. + {command: (.command + " -I" + $cwd + "/misra/include -I" + $cwd + "/build/generated/pico_base -I" + $cwd + "/include")})' build/cppcheck/compile_commands.json > build/cppcheck/compile_commands_patched.json
 
 function check {
     cppcheck \
-        --project=build/cppcheck/compile_commands.json \
+        --project=build/cppcheck/compile_commands_patched.json \
         -j24 \
         --cppcheck-build-dir=build/cppcheck \
         --enable=all \
         --language=c++ \
         --std=c++17 \
-        --addon=.github/scripts/misra.json \
         --force \
         --inline-suppr \
         --xml \
+        -D PICO_CONFIG_RTOS_ADAPTER_HEADER="$cwd/lib/FreeRTOS-Kernel/portable/ThirdParty/GCC/RP2040/include/freertos_sdk_config.h" \
+        -U __CYGWIN__ \
+        -D __GNUC__ \
+        -D __arm__ \
+        -D __ARMEL__ \
+        -D PICO_RP2040 \
+        -D __INTPTR_TYPE__=int -D __INT32_TYPE__="long int" -D __CHAR_BIT__=8 \
+        -U __riscv -U __sparc__ -U __SPU__ -U __MSDOS__ -U __i386__ -U GO32 \
+        -U CYW43_NETUTILS -U LWIP_PERF -U MEMP_USE_CUSTOM_POOLS -U MEMP_USE_CUSTOM_POOLS -U PACK_STRUCT_USE_INCLUDES \
+        -U __CUSTOM_FILE_IO__ -U PICO_CYW43_ARCH_HEADER\
+        --suppress=*:lib/FreeRTOS-Kernel/* \
+        --suppress=*:misra/* \
         -v \
         2> build/cppcheck-result.xml
 }
